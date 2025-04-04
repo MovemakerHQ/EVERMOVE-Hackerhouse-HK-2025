31e525ee6fc92fc52c54519a43281309ef00afad
bernardsimon <bernardziyi@gmail.com>
2025-04-04 20:46:03 +0800

    提交内容

--- 14-Emotion-Apt/packages/go/utils/aptos/aptosClient.go
+++
@@ -1,9 +0,0 @@
-package aptos
-
-import "github.com/aptos-labs/aptos-go-sdk"
-
-client := nil
-client, err := aptos.NewClient(aptos.DevnetConfig)
-if err != nil {
-panic("Failed to create client:" + err.Error())
-}
